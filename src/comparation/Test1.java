6ba73c9238656bcadeece1963f5b7e9d55b2cdc0
ksunandr <ksun.andr@gmail.com>
2022-11-11 18:31:51 +0100

    comparation and generic

---
+++ src/comparation/Test1.java
@@ -0,0 +1,24 @@
+package comparation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("zkseniia");
+        list.add("Ivan");
+        list.add("mari");
+        System.out.println(list);
+
+        Collections.sort(list);
+        System.out.println(list);
+
+
+
+
+
+    }
+}
---
+++ src/generics/Test1NoDiamond.java
@@ -0,0 +1,47 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1NoDiamond {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("1r4444");
+        list1.add("1te");
+  //      list1.add(1);
+        List<Integer> integers = new ArrayList(list1);
+     //   List<Integer> integers = new ArrayLis<>(list1); - this leads to compilation error//
+//error //int i = null;
+//ok //Integer integer = null;
+
+        System.out.println(list1.getClass());
+        System.out.println(integers.getClass());
+
+//        for (String s : list1){
+//            System.out.println(s.length());
+//        }
+//
+//        for (String s : list1){
+//            System.out.println(s.length());
+//        }
+    }
+
+
+     class TestTypeErasure {
+
+        <T extends  InterfaceMy, InterfaceMy1> T test(ArrayList<String> as){
+             ArrayList<T> list = new ArrayList<>();
+             List<Integer> integers1 = new ArrayList<>();
+
+             return list.get(0);
+         }
+
+
+     }
+
+
+     interface InterfaceMy{};
+     interface InterfaceMy1{};
+
+}
