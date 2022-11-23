ad1ecac6bb1f7659efe62d3888c28f262e25d216
ksunandr <ksun.andr@gmail.com>
2022-11-23 17:21:46 +0100

    lists

--- src/collection/CovarianceTest.java
+++ src/collection/CovarianceTest.java
@@ -1,8 +1,7 @@
-package generics;
+package collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class CovarianceTest {
